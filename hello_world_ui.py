e4f98a4af257d813e44a2f8e5ef3cd7f87645e87
KhoaMinhLe <Khoa.le@ocsbstudent.ca>
2017-09-12 15:45:36 -0400

    Commit it please

---
+++ hello_world_ui.py
@@ -0,0 +1,9 @@
+# Created by: Khoa Le
+# Created on September 12 2017
+# Created for ICS3U
+# This program is the "Hello, World!", but with GUI
+
+import ui
+
+v = ui.load_view()
+v.present('sheet')
